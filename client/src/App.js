320cc82b136d70e710829027b0d13c1aece253b4
Martin Todev <mtodev@uwo.ca>
2022-12-01 05:45:28 -0500

    Fixed search by track id

--- client/src/App.js
+++ client/src/App.js
@@ -17,15 +17,15 @@ function App() {
   const ref = useRef();
   const params = useParams();
   const[currentForm,setCurrentForm] = useState('Login')
-console.log(params)
+  console.log(params)
 
-const [track,setMessage] = useState('')
+  const [track,setMessage] = useState('')
 
-const handleChange = e=>{
-  setMessage(e.target.value)
+  const handleChange = e=>{
+    setMessage(e.target.value)
 
-  console.log('Value is: ', e.target.value);
-}
+    console.log('Value is: ', e.target.value);
+  }
 
   const toggleForm = (formName) =>{
       setCurrentForm(formName);
@@ -75,18 +75,14 @@ const handleChange = e=>{
     fetch(`/api/track/trackTitle/${track}`)
     .then(res => res.json()
     .then(data => {
-        const l = document.getElementById('inventory');
-        
-        console.log(data)       
+        const l = document.getElementById('inventory');     
         data.forEach(element =>{
           fetch(`/api/tracks/${element}`)
             .then(res => res.json()
-            .then(trackData => {
-              trackData.forEach(e => {
-                const item = document.createElement('li');//need to add a list
-                item.appendChild(document.createTextNode(`Track_ID: ${e.track_id}, Track_Title: ${e.track_title}`));     
-                l.appendChild(item)
-              })
+            .then(data => {
+              const item = document.createElement('li');//need to add a list
+              item.appendChild(document.createTextNode(`Track_ID: ${data.track_id}, Track_Title: ${data.track_title}`));     
+              l.appendChild(item);
             })
           )
         })
@@ -103,20 +99,15 @@ const handleChange = e=>{
     <div>
        <Header />
       {
-        currentForm == "Login" ? <Login onFormSwitch = {toggleForm} /> : <Register onFormSwitch = {toggleForm}/>
+        currentForm === "Login" ? <Login onFormSwitch = {toggleForm} /> : <Register onFormSwitch = {toggleForm}/>
         &&
-        currentForm == "Body" ? <Body onFormSwitch = {toggleForm}/> : <Register onFormSwitch = {toggleForm}/>
+        currentForm === "Body" ? <Body onFormSwitch = {toggleForm}/> : <Register onFormSwitch = {toggleForm}/>
       }
       {
         <div className = "app">
       <span>
-<<<<<<< HEAD
-      <input type = "text" id = "track" placeholder = "Search by Track Name" className = "search" name ="track"  onChange={handleChange} value = {track}></input>
-      <button className = "trackBtn" id = "searchTrack">Search</button>
-=======
       <input type = "text" id = "track" placeholder = "Search by Track Name" className = "search" name ="track" onChange={handleChange} value = {track} />
       <button className = "trackBtn" id ="searchTrack" onClick={handleSubmit}>Search</button>
->>>>>>> 0dc4c5151434ee4d6f7bb0ff665483e28f6c05e6
       <ol id = "inventory"></ol>
       </span>
       </div>
