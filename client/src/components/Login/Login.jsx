d6c88bf1c8da10a01beaa34a582ff25bc5c091c7
piran777 <paminul@uwo.ca>
2022-12-07 16:32:48 -0500

    added the dont have account function with route

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,13 +1,32 @@
-import React, {useState} from 'react';
-
+import React from 'react'
+import { useState } from 'react';
+import {useNavigate} from "react-router-dom";
+import {Route} from "react-router-dom";
+import axios from 'axios';
 export default function Login(props) {
   const[email,setEmail] = useState(""); //set email initally empty
   const[pass,setPass] = useState("");
-  
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
   const handleSubmit = (e) =>{
     e.preventDefault();
     console.log(email + pass);
   }
+
+
+  const change = async (e) =>{
+    e.preventDefault();
+    try{
+        setTimeout(function() {
+        navigate ('/register')
+      }, 1000);
+    }
+    catch(error){
+        setError(error.response.data.error);
+    }
+}
+
   
   return(
     <div className = "auth-container"> 
@@ -18,7 +37,7 @@ export default function Login(props) {
           <label htmlFor = "password">password</label>
           <input value={pass} onChange={(e)=>setPass(e.target.value)} type = "password" placeholder ="*******" id="password" name="password"/>
           <button type = "submit" onClick={() => props.onFormSwitch('Body')}>Log In</button> 
-          <button className="linkBtn" onClick={() => props.onFormSwitch('Register')}>Don't have an account? Register</button>
+          <button className="linkBtn" onClick = {change}>Don't have an account? Register</button>
       </form>
     </div>
   )
